f4922b2da3099a5e9d13600fa15802d4b9588e28
Varun <varun.g.dsu@gmail.com>
2024-07-20 15:59:10 +0530

    The flex container was not centered, edited the file to make it centered

--- front-end/src/pages/CreateBook.jsx
+++ front-end/src/pages/CreateBook.jsx
@@ -4,7 +4,6 @@ import Spinner from '../components/Spinner';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-
 const CreateBook = () => {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
@@ -14,12 +13,12 @@ const CreateBook = () => {
 
   const handleSaveBook = () => {
     const data = {
-      title, 
+      title,
       author,
       publishYear
     };
     setLoading(true);
-    axios 
+    axios
       .post('http://localhost:5555/books', data)
       .then(() => {
         setLoading(false);
@@ -27,48 +26,54 @@ const CreateBook = () => {
       })
       .catch((error) => {
         setLoading(false);
-        alert('An error occured. Please check the console for more information ');
+        alert('An error occurred. Please check the console for more information.');
         console.log(error);
       });
   };
+
   return (
-    <div className='p-4'>
-      <Backbutton />
-      <h1 className='text-3xl my-4'>Create Book</h1>
+    <div className='flex flex-col min-h-screen p-4'>
+      <div className='w-full max-w-md '>
+        <Backbutton />
+        <h1 className='text-3xl my-4'>Create Book</h1>
+      </div>
       {loading ? <Spinner /> : ''}
-      <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx auto'>
-        <div className='my-4'>
-          <label className='text-xl mr-4 text-grey-500'>Title</label>
-          <input 
-            type="text" 
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2 w-full'
-          />
+      <div className='flex my-10 justify-center w-full'>
+        <div className='flex flex-col border-2 border-sky-400 rounded-xl w-full max-w-md p-4'>
+          <div className='my-4'>
+            <label className='text-xl mr-4 text-gray-500'>Title</label>
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              className='border-2 border-gray-500 px-4 py-2 w-full'
+            />
+          </div>
+          <div className='my-4'>
+            <label className='text-xl mr-4 text-gray-500'>Author</label>
+            <input
+              type="text"
+              value={author}
+              onChange={(e) => setAuthor(e.target.value)}
+              className='border-2 border-gray-500 px-4 py-2 w-full'
+            />
+          </div>
+          <div className='my-4'>
+            <label className='text-xl mr-4 text-gray-500'>Year of Publish</label>
+            <input
+              type="text"
+              value={publishYear}
+              onChange={(e) => setPublishYear(e.target.value)}
+              className='border-2 border-gray-500 px-4 py-2 w-full'
+            />
+          </div>
+          <button className='p-2 bg-sky-300 m-8' onClick={handleSaveBook}>
+            Save
+          </button>
         </div>
-        <div className='my-4'>
-          <label className='text-xl mr-4 text-grey-500'>Author</label>
-          <input 
-            type="text" 
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2 w-full'
-          />
-        </div>
-        <div className='my-4'>
-          <label className='text-xl mr-4 text-grey-500'>Year of Publish</label>
-          <input 
-            type="text" 
-            value={publishYear}
-            onChange={(e) => setPublishYear(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2 w-full'
-          />
-        </div>
-        <button className='p-2 bg-sky-300 m-8' onClick={handleSaveBook}>
-          Save
-        </button>
       </div>
     </div>
   );
 }
+
 export default CreateBook;
