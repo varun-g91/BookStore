e22112cbd0c62b4336e5a2657ef97b59f58c39fd
Varun <varun.g.dsu@gmail.com>
2024-07-22 11:17:41 +0530

    Lifter showType and setShowType up to App component and passed them down as props to Navbad and Home components

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CreateBook from './pages/CreateBook';
@@ -6,12 +6,16 @@ import ShowBook from './pages/ShowBook';
 import EditBook from './pages/EditBook';
 import DeleteBook from './pages/DeleteBook';
 import { ShowTypeProvider } from './components/ShowTypeContext';
+import Navbar from './components/home/Navbar';
 
 const App = () => {
+  const [showType, setShowType] = useState('table');
+
   return (
     <ShowTypeProvider>
+      <Navbar setShowType={setShowType}/>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Home showType={showType} setShowType={setShowType}  />} />
         <Route path='/books/create' element={<CreateBook />} />
         <Route path='/books/details/:id' element={<ShowBook />} />
         <Route path='/books/edit/:id' element={<EditBook />} />
